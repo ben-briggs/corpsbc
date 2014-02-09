0a7c108353850f6cc59553b37d51b24ed8e51bd2
Ben Briggs <benhelite@gmail.com>
2014-02-09 14:52:40 -0600

    Fourth commit test

---
+++ selenium_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+__author__ = 'ben'
+
+import timeit
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from pyvirtualdisplay import Display
+import time
+
+print 'Initializing display...'
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+print 'Starting Firefox...'
+browser = webdriver.Firefox()
+
+
+def test():
+    print 'Initializing test...'
+    browser.get('http://www.google.com')
+    time.sleep(1)
+    q = browser.find_element_by_name('q')
+    q.send_keys("Jack's Family Restaurants")
+    q.send_keys(Keys.ENTER)
+
+    time.sleep(1)
+
+    results = browser.find_elements_by_class_name('web_result')
+    for result in results:
+        try:
+            print '-' * 80
+            print result.text
+        except:
+            pass
+    return 0
+
+#test()
+print(timeit.timeit("test()", setup="from __main__ import test", number=10))
+print("Hello World")
+browser.close()
+display.stop()
